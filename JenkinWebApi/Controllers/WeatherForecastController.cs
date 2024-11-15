82a2109c153a66394ced1d8540f04cb036c5e31b
navas <navas_17m@yahoo.com>
2024-11-15 19:41:07 +0530

    commit 1

--- JenkinWebApi/Controllers/WeatherForecastController.cs
+++ JenkinWebApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace JenkinWebApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Test"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
