c4f564c55833bf6334ff4224b2689da9dd6ded68
navas <navas_17m@yahoo.com>
2024-11-15 20:13:52 +0530

    test

--- JenkinWebApi/Controllers/WeatherForecastController.cs
+++ JenkinWebApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace JenkinWebApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Test"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -21,7 +21,7 @@ namespace JenkinWebApi.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 2).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
